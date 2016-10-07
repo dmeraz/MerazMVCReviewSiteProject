69d4d5061fb78c0477869ad6717c469e9799d7e8
dmeraz <darin.meraz@gmail.com>
2016-10-07 10:14:53 -0400

    first commit, added Review Model

---
+++ MerazMVCReviewSiteProject/Models/Review.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MerazMVCReviewSiteProject.Models
+{
+    public class Review
+    {
+        //Properties
+        [Key]
+        //ID
+        public interface ID { get; set; }
+        //Title
+        //Content
+        //Publish Date
+        //Etc
+    }
+}
